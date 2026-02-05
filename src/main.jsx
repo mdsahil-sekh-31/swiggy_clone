9bac0e5d8b943961d425db5804c7bb87fc6d4736
Md Sahil Sekh <mdsahilsekh31@gmail.com>
2026-02-06 02:14:00 +0530

    hasrhouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router";
 import App from "./App";
 import Restaurants from "./Restaurants";
 import RestaurantMenu from "./RestaurantMenu";
 createRoot(document.getElementById("root")).render(
   
-      <BrowserRouter  basename="/swiggy_clone">
+      <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/restaurant" element={<Restaurants />} />
       <Route path="/city/kolkata/:id" element={<RestaurantMenu/>}/>
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
 );
